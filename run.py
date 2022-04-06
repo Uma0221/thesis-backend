a71bb5e041c46fc774c5ff93ec17f44bf2c1c30b
uma <sarapan19980221@gmail.com>
2022-04-07 02:08:42 +0800

    feat: retrun index

--- run.py
+++ run.py
@@ -13,14 +13,17 @@ def index():
 
 @app.route('/test', methods=['GET'])
 def getResult():
+    index = 10
+
     input = np.array([[0, 0, 0.6, 0, False, 0]])
     result = model.predict(input)
-    return jsonify({'result': str(result)})
+    return jsonify({'index': index,'predict': result})
 
 @app.route('/predict', methods=['POST'])
 def postInput():
     # 取得前端傳過來的數值
     insertValues = request.get_json()
+    index = insertValues['index']
     x1=insertValues['content_length']
     x2=insertValues['photos_count']
     x3=insertValues['star_gap']
@@ -30,7 +33,7 @@ def postInput():
 
     input = np.array([[x1, x2, x3, x4, x5, x6]])
     result = model.predict(input)
-    return jsonify({'return': str(result)})
+    return jsonify({'index': index,'predict': result})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=False)

034b4ffc8bfe92fbfa7d2d5f2dec79e206473290
uma <sarapan19980221@gmail.com>
2022-04-10 00:28:23 +0800

    feat: predict array which with error values v2

--- run.py
+++ run.py
@@ -15,24 +15,24 @@ def index():
 def getResult():
     predictArr = []
 
-    insertValuesArr = np.array([[0, 0, 0, 0.6, 0, False, 0],[1, 0, 0, 0.6, 0, False, 0],[2],[]])
+    insertValuesArr = np.array([[0, 0, 0, 0.6, 0, False, 0],[-1,4,2],[1, 0, 0, 0.6, 0, False, 0],[],[2,3],[10],[-2],[3,5,4,0,2,3,9,1,8]])
     for insertValues in insertValuesArr:
-        if(len(insertValues)>0 and len(insertValues)!=7):
-            index = insertValues[0]
-            result = str(-1)
+        if(len(insertValues)>0 and insertValues[0]>=0 and insertValues[0]<=9):
+            if(len(insertValues)!=7):
+                index = insertValues[0]
+                result = -1
 
-            predictArr.append({'index': str(index),'predict': str(result)})
-        elif(len(insertValues)==7):
-            index = insertValues[0]
-            x1=insertValues[1]
-            x2=insertValues[2]
-            x3=insertValues[3]
-            x4=insertValues[4]
-            x5=insertValues[5]
-            x6=insertValues[6]
+            else:
+                index = insertValues[0]
+                x1=insertValues[1]
+                x2=insertValues[2]
+                x3=insertValues[3]
+                x4=insertValues[4]
+                x5=insertValues[5]
+                x6=insertValues[6]
 
-            input = np.array([[x1, x2, x3, x4, x5, x6]])
-            result = model.predict(input)
+                input = np.array([[x1, x2, x3, x4, x5, x6]])
+                result = model.predict(input)
         
             predictArr.append({'index': str(index),'predict': str(result)})
 
@@ -44,22 +44,21 @@ def postInput():
     # 取得前端傳過來的數值
     insertValuesArr = request.get_json()
     for insertValues in insertValuesArr:
-        if(len(insertValues)>0 and len(insertValues)!=7):
-            index = insertValues['index']
-            result = str(-1)
+        if(len(insertValues)>0 and insertValues[0]>=0 and insertValues[0]<=9):
+            if(len(insertValues)!=7):
+                index = insertValues['index']
+                result = -1
+            else:
+                index = insertValues['index']
+                x1=insertValues['content_length']
+                x2=insertValues['photos_count']
+                x3=insertValues['star_gap']
+                x4=insertValues['like_count']
+                x5=insertValues['reply']
+                x6=insertValues['reviewer_rank']
 
-            predictArr.append({'index': str(index),'predict': str(result)})
-        elif(len(insertValues)==7):
-            index = insertValues['index']
-            x1=insertValues['content_length']
-            x2=insertValues['photos_count']
-            x3=insertValues['star_gap']
-            x4=insertValues['like_count']
-            x5=insertValues['reply']
-            x6=insertValues['reviewer_rank']
-
-            input = np.array([[x1, x2, x3, x4, x5, x6]])
-            result = model.predict(input)
+                input = np.array([[x1, x2, x3, x4, x5, x6]])
+                result = model.predict(input)
         
             predictArr.append({'index': str(index),'predict': str(result)})
 
